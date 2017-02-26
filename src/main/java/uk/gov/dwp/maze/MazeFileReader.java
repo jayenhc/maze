e041bc472a05c7a902bd5d1a4c38438bcb0c6f8a
jayenhc@yahoo.com <jayenhc@yahoo.com>
2017-02-26 11:53:41 +0000

    Maze initial commit

---
+++ src/main/java/uk/gov/dwp/maze/MazeFileReader.java
@@ -0,0 +1,20 @@
+package uk.gov.dwp.maze;
+
+import java.io.File;
+
+/**
+ * Created by jchondig on 25/02/2017.
+ */
+public class MazeFileReader {
+
+    private File file;
+
+    public MazeFileReader(String filePath) {
+        try {
+            this.file = new File(filePath);
+        } catch (NullPointerException npe) {
+            throw new IllegalArgumentException("File path cannot be null.");
+        }
+    }
+
+}
